8ec6627be8f2f2b34f06f79b72408df912485a9a
sprakash1729 <shashi50prakash@gmail.com>
2025-01-08 00:50:43 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,38 +1,14 @@
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:gametrial.database.windows.net,1433; Database = gametrialdb", "gametrial_root", "Pass@123");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-?>
-<?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-  // Connect to the database
-  $conn = new PDO("sqlsrv:server = tcp:gametrial.database.windows.net,1433; Database = gametrialdb", "gametrial_root", "Pass@123");
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$server = getenv("AZURE_SQL_SERVERNAME");
+$database = getenv("AZURE_SQL_DATABASE");
+$user = getenv("AZURE_SQL_UID");
+$password = getenv("AZURE_SQL_PASSWORD");
 
-  // Sample query to check connection
-  $sql = "SELECT * from dbo.users AS test_value";
-  $stmt = $conn->prepare($prepared_stmt);
-  $stmt->execute();
+$connectionOptions = array(
+    "Database" => gametraildb,
+    "Uid" => gametrial_root,
+    "PWD" => Pass@123
+);
 
-  // Check if query execution was successful
-  if ($stmt->rowCount() > 0) {
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    echo "Connection successful! Test value: " . $result['test_value'];
-  } else {
-    echo "Query execution failed.";
-  }
-} catch (PDOException $e) {
-  echo "Error connecting to SQL Server: " . $e->getMessage();
-  die();
-} finally {
-  // Close the connection (optional, but recommended)
-  $conn = null;
-}
+$conn = sqlsrv_connect($serverName, $connectionOptions);
 ?>
