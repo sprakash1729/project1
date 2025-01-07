930945808444dea64ff205927d803343013f14fc
sprakash1729 <shashi50prakash@gmail.com>
2025-01-07 21:29:11 +0530

    DB Connection

--- config.php
+++ config.php
@@ -1,20 +1,16 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', '91club');
-
-$conn = @mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME); // The @ suppresses warnings
-
-if (!$conn) {
-    echo "<h1>Database Connection Error</h1>"; // More user-friendly message
-    echo "<p>Error Number: " . mysqli_connect_errno() . "</p>"; // Display error number
-    echo "<p>Error Message: " . mysqli_connect_error() . "</p>"; // Display error message
-    // Optionally log the error to a file for debugging in production
-    error_log("Database connection error: " . mysqli_connect_error(), 0);
-    die(); // Stop execution
-} else {
-    // Remove in production, only for testing
- //   echo "<h1>Database connection established successfully!</h1>"; // Confirmation message
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:gametrial.database.windows.net,1433; Database = gametrialdb", "gametrial_root", "Pass@123");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "gametrial_root", "pwd" => "Pass@123", "Database" => "gametrialdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:gametrial.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
